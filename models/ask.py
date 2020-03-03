c508ccfa21dfd51470f5b858285e62605c4d9655
Jochen Mattes <jochen.mattes@werk24.biz>
2020-03-03 18:27:21 +0100

    merged asks

--- models/ask.py
+++ models/ask.py
@@ -1,5 +1,14 @@
 from pydantic import BaseModel
-from .ask_type import W24AskType
+
+
+from enum import Enum
+
+
+class W24AskType(str, Enum):
+    THUMBNAIL_PAGE = "THUMBNAIL_PAGE"
+    THUMBNAIL_SHEET = "THUMBNAIL_SHEET"
+    THUMBNAIL_DRAWING = "THUMBNAIL_DRAWING"
+    MEASURES = "MEASURES"
 
 
 class W24Ask(BaseModel):
@@ -7,3 +16,55 @@ class W24Ask(BaseModel):
     in a W24Demand
     """
     ask_type: W24AskType
+
+
+class W24AskThumbnail(W24Ask):
+    """ Base model for features that request a
+    thumbnail.
+
+    maximal_width and maximal_height describe the maximal
+    dimensions of the thumbnail (in pixel).
+
+    Beware that the resulting image dimensions depend
+    on many factors, such as:
+    (i) resolution of the original image,
+    (ii) rotation of the sheet on the page,
+    (iii) noise level of the image, etc.
+
+    Setting auto_rotate to True will rotate the image for
+    "human-consumption" (the human rotation algorithm is
+    more complext than you'd expect)
+    """
+    # maximal_width: int = 512
+    # maximal_height: int = 512
+    # auto_rotate: bool = False
+
+
+class W24AskThumbnailPage(W24AskThumbnail):
+    """ Requesting the W24DrawingReadFeatureThumbnailPage will add
+    a thumbnail of the complete page including the border area
+    to the result.
+    """
+    ask_type = W24AskType.THUMBNAIL_PAGE
+
+
+class W24AskThumbnailSheet(W24AskThumbnail):
+    """ Requesting the W24DrawingReadFeatureThumbnailPage will add
+    a thumbnail of the complete page including the border area
+    to the result.
+    """
+    ask_type = W24AskType.THUMBNAIL_SHEET
+
+
+class W24AskThumbnailDrawing(W24AskThumbnail):
+    """ Requesting the W24DrawingReadFeatureThumbnailPage will add
+    a thumbnail of the complete page including the border area
+    to the result.
+    """
+    ask_type = W24AskType.THUMBNAIL_DRAWING
+
+
+class W24AskMetadataOuterDimensions(W24Ask):
+    """ Requesting the W24AskMetadataOuterDimensions will
+    extract the outer dimensions of the extracted part
+    """
--- models/ask_measures.py
+++
@@ -1,9 +0,0 @@
-from .ask import W24Ask
-from .ask_type import W24AskType
-
-
-class W24AskMeasures(W24Ask):
-    """ Requesting the W24AskMeasures will add
-    a list of all detected measures to the result
-    """
-    ask_type = W24AskType.MEASURES
