9f40423c07325c6af435f95d74fbb969387fc93b
Jochen Mattes <jochen.mattes@werk24.biz>
2020-03-06 10:13:33 +0100

    Added overall dimensions

---
+++ models/overall_dimensions.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from pydantic import BaseModel
+from .measure import W24Measure
+
+
+class W24OverallDimensionsShape(str, Enum):
+    ROD_ROUND = "ROD_ROUND"
+    ROD_HEXAGON = "ROD_HEXAGON"
+    ROD_SQUARE = "ROD_SQUARE"
+
+
+class W24OverallDimensions(BaseModel):
+    shape: W24OverallDimensionsShape
+    x_measure: W24Measure
+    y_measure: W24Measure
+    z_measure: W24Measure
