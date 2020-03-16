74bdfa317d51cb3f9c7cbba920a9d0b736f90f12
Jochen Mattes <jochen.mattes@werk24.biz>
2020-03-16 19:18:15 +0100

    pushes version to 0.1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="werk24",
-    version="0.1.5",
+    version="0.1.6",
     author="Jochen Mattes",
     packages=["werk24", "werk24.models"],
     install_requires=["websockets"]
