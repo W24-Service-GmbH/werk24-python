23f868a9aa63dc702df47e49ac816eb63aee12f1
Jochen Mattes <jochen.mattes@werk24.biz>
2020-03-23 16:11:56 +0100

    softens aioboto3 requirements to >= 6.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Jochen Mattes",
     packages=["werk24", "werk24.models", "werk24.cli"],
     install_requires=[
-        "aioboto3 >= 6.5.0",
+        "aioboto3 >= 6.4.1",
         "pydantic >= 1.4",
         "python-dotenv>=0.10.1",
         "websockets >= 8.1"],
