6c9ffcd8b9db731da9e52239cef3deb7e2c94477
Jochen Mattes <jochen.mattes@werk24.biz>
2020-03-25 15:55:28 +0100

    makes werk24.W24TechreadClient available

--- werk24/__init__.py
+++ werk24/__init__.py
@@ -1,2 +1,2 @@
 from .models.ask import *
-from .techread_client import Hook
+from .techread_client import Hook, W24TechreadClient
---
+++ werk24/integration_tests/techread_itest.py
@@ -0,0 +1,90 @@
+import mock
+import unittest
+from werk24.techread_client import W24TechreadClient, W24TechreadArchitecture, W24TechreadArchitectureStatus, W24TechreadMessageType
+from werk24.models.ask import *
+from dotenv import load_dotenv
+import aiounittest
+import os
+from pathlib import Path
+
+
+class TechreadIntegrationTest(aiounittest.AsyncTestCase):
+
+    """ List of the environment variables that
+    are required by the application
+    """
+    envs_required = [
+        "W24TECHREAD_SERVER_HTTPS",
+        "W24TECHREAD_SERVER_WSS",
+        "W24TECHREAD_VERSION",
+        "W24TECHREAD_AUTH_REGION",
+        # "W24TECHREAD_AUTH_IDENTITY_POOL_ID",
+        "W24TECHREAD_AUTH_CLIENT_ID",
+        "W24TECHREAD_AUTH_CLIENT_SECRET",
+        "W24TECHREAD_AUTH_USERNAME",
+        "W24TECHREAD_AUTH_PASSWORD"]
+
+    def setUp(self):
+        self._load_env()
+        self.client = W24TechreadClient.make_from_env()
+        assert isinstance(self.client, W24TechreadClient)
+
+        # set the drawing path
+        base_path = Path(os.path.dirname(os.path.realpath(__file__)))
+        self.drawing_path = base_path / "fixtures" / "techdraw.png"
+
+    def _load_env(self):
+        """ load the environment variables and test that they
+        are available
+        """
+        load_dotenv()
+        for cur_env in self.envs_required:
+            self.assertIsNotNone(os.environ.get(cur_env))
+
+    def _get_drawing_bytes(self):
+        with open(self.drawing_path, "rb") as drawing_handle:
+            return drawing_handle.read()
+
+    def _make_asks_list(self):
+        return [
+            W24AskPageThumbnail()
+        ]
+
+    async def test_techread(self):
+
+        # start a session
+        async with self.client as session:
+
+            # check whether we have obtained a token
+            self.assertIsNotNone(session._auth_client.token)
+
+            # get the architecture status
+            architecture_status = await session.get_architecture_status(
+                W24TechreadArchitecture.GPU_V1)
+
+            # check whether that value is valid
+            self.assertIsInstance(
+                architecture_status,
+                W24TechreadArchitectureStatus)
+
+            # now get the file
+            drawing_bytes = self._get_drawing_bytes()
+
+            asks_list = []  # self._make_asks_list()
+
+            # and make the request
+            is_first_message = True
+            async for message in session.read_drawing(drawing_bytes, asks_list):
+                print(message)
+
+                # the first message needs to be a TECHREAD_STARTED
+                if is_first_message:
+                    self.assertEqual(
+                        message.message_type,
+                        W24TechreadMessageType.TECHREAD_STARTED)
+                    is_first_message = False
+
+                # if the message tells us about an error, we nee to raise it
+                self.assertNotEqual(
+                    message.message_type,
+                    W24TechreadMessageType.ERROR_INTERNAL)
