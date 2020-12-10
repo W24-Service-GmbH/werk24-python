379ade2661798f8caa67a5d50adfd8ea4257d0ff
Jochen Mattes <jochen.mattes@werk24.biz>
2020-12-10 22:01:02 +0100

    fixes vulnerability in test

--- test/cli_auth_test.py
+++ test/cli_auth_test.py
@@ -1,16 +1,30 @@
+from typing import List
+import sys
 import subprocess
 import unittest
 
 class TestCliAuth(unittest.TestCase):
 
+    @staticmethod
+    def _call_cli(command:List[str])-> str:
+
+        # get the absolute path of the python executable
+        # to stay safe
+        executable = sys.executable
+
+        # then make the call and return the result
+
+        return subprocess.check_output([
+            executable,
+            "-m", "werk24.cli.w24cli"
+            ] + command
+        ).decode()
+
+
     def test_ask_jwt_token(self):
 
-        result = subprocess.check_output([
-            "python",
-            "-m",
-            "werk24.cli.w24cli",
-            "auth",
-            "--ask-jwt-token"]).decode()
+        # call!
+        result = self._call_cli(["auth","--ask-jwt-token"])
         
         # assume that we receive a very simple response
         parts = result.split(":", 1)
