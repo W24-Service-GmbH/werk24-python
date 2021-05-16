948117b8e293245550aa847cc6dfba38e59e113a
werk24 <jochen.mattes@werk24.biz>
2021-05-16 12:39:59 +0200

    updates setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ VERSIONFILE = "werk24/_version.py"
 NAME = "werk24"
 
 
-def _get_version(version_file: str)->str:
+def _get_version(version_file: str) -> str:
     version_pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
     with open(version_file, "rt") as file_handle:
         match = re.search(version_pattern, file_handle.read(), re.M)
@@ -32,7 +32,7 @@ setup(
     author_email="info@werk24.io",
     description="Werk24 Client to read PDF- and Image-based "
     "Technical Drawings / Engineering Drawings",
-    url="https://www.werk24.io",
+    url="https://werk24.io",
     entry_points={
         "console_scripts": [
             "w24cli=werk24.cli.w24cli:main",
@@ -62,9 +62,19 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Image Recognition"],
+    keywords=[
+        'Digitisation', 'Digitization', 'Engineering Drawing',
+        'Engineering Drawings', 'Technical Drawing', 'Technical Drawings',
+        'CAD', 'CAD Drawing', 'Data Extraction', 'Information Extraction',
+        'Model Based Definition', 'EN10027', 'ISO2768', 'Title Block',
+        'General Tolerances', 'Material', 'Drawing ID', 'Drawing Designation',
+        'Product Manufacturing Information', 'PMI', 'Scanned Document',
+        'Bill of Material', 'BOM', 'Anonymiziation', 'RFQ', 'GD&T',
+        'General Dimensioning and Toleration', 'Vectorization'
+    ],
     python_requires='>=3.7.1',
     project_urls={
-        "Documentation": "https://werk24.io/docs/index.html"
+        "Documentation": "https://docs.werk24.io/"
     },
     long_description_content_type='text/markdown',
     long_description=_get_description(),
