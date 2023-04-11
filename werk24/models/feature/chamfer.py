0b6f4fcd35bb308c0329ebdfbeaefb98b4f4a629
werk24 <jochen.mattes@werk24.biz>
2023-04-11 13:08:43 +0200

    moves tolerances

--- werk24/models/feature/chamfer.py
+++ werk24/models/feature/chamfer.py
@@ -1,12 +1,15 @@
 from werk24.models.feature.base import W24Feature
 from typing import Literal
 from werk24.models.size import W24Size, W24SizeTolerance
-from werk24.models.angle import W24Angle, W24AngleTolerance
+from werk24.models.angle import W24AngleLabel, W24AngleTolerance
+
+
+class W24ValueToleranced()
 
 
 class W24FeatureChamfer(W24Feature):
     type: Literal["CHAMFER"] = "CHAMFER"
     size: W24Size
     size_tolerance: W24SizeTolerance
-    angle: W24Angle
+    angle: W24AngleLabel
     angle_tolerance: W24AngleTolerance
--- werk24/models/value.py
+++ werk24/models/value.py
@@ -4,36 +4,32 @@ from typing import Optional
 from pint import Quantity, UnitRegistry
 from pydantic import BaseModel
 
+from werk24.models.size import W24SizeTolerance
+
 ureg = UnitRegistry()
 
 
 class W24PhysicalQuantity(BaseModel):
+    """Physical Quantity.
+
+    Physical Quantity with a value, unit and tolerance.
+
+    Attributes:
+        blurb (str): Blurb of the Physical Property for
+            human consumption.
+        value (Quantity): Physical quantity in the string
+            format of pint.
+        tolerance (Optional[W24Tolerance]): Tolerance
+    """
     class Config:
         arbitrary_types_allowed = True
 
+    blurb: str
     value: Quantity
-
-    def __str__(self) -> str:
-        return str(self.value)
-
-
-class W24PhysicalQuantityRange(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
-
-    min_value: Quantity
-    max_value: Optional[Quantity]
-
-    def __str__(self) -> str:
-        if self.max_value is None:
-            return str(self.min_value)
-        return f"{self.min_value} - {self.max_value}"
-
-
-class W24ValueRange(BaseModel):
-    min_value: Decimal
-    max_value: Decimal
+    tolerance: Optional[W24SizeTolerance] = None
 
 
 class W24Value(BaseModel):
+    blurb: str
     value: Decimal
+    tolerance: Optional[W24SizeTolerance] = None
