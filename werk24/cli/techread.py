ec4b4358e94f0a29f14d8d83e6a108ab67b6dcbc
werk24 <jochen.mattes@werk24.biz>
2023-09-05 19:20:28 +0200

    adds hooks for part family characterization

--- werk24/cli/techread.py
+++ werk24/cli/techread.py
@@ -8,13 +8,14 @@ from collections import namedtuple
 from typing import Any, Dict, List, Optional
 
 from dotenv import load_dotenv
-from pydantic import UUID4, ValidationError
+from pydantic import UUID4
 
 from werk24.cli import utils
 from werk24.exceptions import RequestTooLargeException
 from werk24.models.ask import (
     W24AskCanvasThumbnail,
     W24AskPageThumbnail,
+    W24AskPartFamilyCharacterization,
     W24AskSectionalThumbnail,
     W24AskSheetAnonymization,
     W24AskSheetThumbnail,
@@ -42,8 +43,9 @@ load_dotenv(".werk24")
 # We recommend using logging.WARNING for production
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',  # noqa
-    datefmt='%Y-%m-%d %H:%M:%S')
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",  # noqa
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 # get the local logger
 logger = logging.getLogger(__name__)  # pylint:disable = invalid-name
@@ -53,66 +55,73 @@ logger = logging.getLogger(__name__)  # pylint:disable = invalid-name
 HookConfig = namedtuple("HookConfig", "arg ask function")
 hook_config = [
     HookConfig(
-        'ask_page_thumbnail',
+        "ask_page_thumbnail",
         W24AskPageThumbnail,
-        lambda m: _show_image("Page Thumbnail", m.payload_bytes)),
-    HookConfig(
-        'ask_sheet_thumbnail',
-        W24AskSheetThumbnail,
-        lambda m: _show_image("Sheet Thumbnail", m.payload_bytes)),
-    HookConfig(
-        'ask_sheet_anonymization',
-        W24AskSheetAnonymization,
-        lambda m: _show_image("Sheet Anonymization", m.payload_bytes)
+        lambda m: _show_image("Page Thumbnail", m.payload_bytes),
     ),
     HookConfig(
-        'ask_canvas_thumbnail',
-        W24AskCanvasThumbnail,
-        lambda m: _show_image("Canvas Thumbnail", m.payload_bytes)),
+        "ask_sheet_thumbnail",
+        W24AskSheetThumbnail,
+        lambda m: _show_image("Sheet Thumbnail", m.payload_bytes),
+    ),
     HookConfig(
-        'ask_sectional_thumbnail',
+        "ask_sheet_anonymization",
+        W24AskSheetAnonymization,
+        lambda m: _show_image("Sheet Anonymization", m.payload_bytes),
+    ),
+    HookConfig(
+        "ask_canvas_thumbnail",
+        W24AskCanvasThumbnail,
+        lambda m: _show_image("Canvas Thumbnail", m.payload_bytes),
+    ),
+    HookConfig(
+        "ask_sectional_thumbnail",
         W24AskSectionalThumbnail,
-        lambda m: _show_image("Sectional Thumbnail", m.payload_bytes)),
+        lambda m: _show_image("Sectional Thumbnail", m.payload_bytes),
+    ),
     # HookConfig(
     #     'ask_variant_angles',
     #     W24AskVariantAngles,
     #     lambda m: _print_payload("Ask Variant Angles", m.payload_dict)),
     HookConfig(
-        'ask_variant_external_dimensions',
+        "ask_variant_external_dimensions",
         W24AskVariantExternalDimensions,
-        lambda m: _print_payload("Ask Variant Ext. Dimensions", m.payload_dict)),
-    HookConfig(
-        'ask_variant_gdts',
-        W24AskVariantGDTs,
-        lambda m: _print_payload("Ask Variant GDTs", m.payload_dict)),
-    HookConfig(
-        'ask_variant_measures',
-        W24AskVariantMeasures,
-        lambda m: _print_payload("Ask Variant Measures", m.payload_dict)),
-    HookConfig(
-        'ask_variant_radii',
-        W24AskVariantRadii,
-        lambda m: _print_payload("Ask Variant Radii", m.payload_dict)),
-    HookConfig(
-        'ask_variant_roughnesses',
-        W24AskVariantRoughnesses,
-        lambda m: _print_payload("Ask Variant Roughnesses", m.payload_dict)),
-    HookConfig(
-        'ask_variant_cad',
-        W24AskVariantCAD,
-        lambda m: _store_variant_cad(
-            m.payload_dict,
-            m.payload_bytes,
-            m.exceptions)),
-    HookConfig(
-        'ask_titleblock',
-        W24AskTitleBlock,
-        lambda m: _print_payload("Ask TitleBlock", m.payload_dict)
+        lambda m: _print_payload("Ask Variant Ext. Dimensions", m.payload_dict),
     ),
     HookConfig(
-        'ask_variant_thread_elements',
+        "ask_variant_gdts",
+        W24AskVariantGDTs,
+        lambda m: _print_payload("Ask Variant GDTs", m.payload_dict),
+    ),
+    HookConfig(
+        "ask_variant_measures",
+        W24AskVariantMeasures,
+        lambda m: _print_payload("Ask Variant Measures", m.payload_dict),
+    ),
+    HookConfig(
+        "ask_variant_radii",
+        W24AskVariantRadii,
+        lambda m: _print_payload("Ask Variant Radii", m.payload_dict),
+    ),
+    HookConfig(
+        "ask_variant_roughnesses",
+        W24AskVariantRoughnesses,
+        lambda m: _print_payload("Ask Variant Roughnesses", m.payload_dict),
+    ),
+    HookConfig(
+        "ask_variant_cad",
+        W24AskVariantCAD,
+        lambda m: _store_variant_cad(m.payload_dict, m.payload_bytes, m.exceptions),
+    ),
+    HookConfig(
+        "ask_titleblock",
+        W24AskTitleBlock,
+        lambda m: _print_payload("Ask TitleBlock", m.payload_dict),
+    ),
+    HookConfig(
+        "ask_variant_thread_elements",
         W24AskVariantThreadElements,
-        lambda m: _print_payload("Ask Variant Thread Elements", m.payload_dict)
+        lambda m: _print_payload("Ask Variant Thread Elements", m.payload_dict),
     ),
 ]
 
@@ -120,9 +129,9 @@ hook_config = [
 def _store_variant_cad(
     payload_dict: Dict[str, Any],
     payload_bytes: Optional[bytes],
-    exceptions: List[W24TechreadException]
+    exceptions: List[W24TechreadException],
 ) -> None:
-    """ Store the CAD file the current directory
+    """Store the CAD file the current directory
 
     Args:
         payload_dict (Dict[str, Any]): Payload Dictionary
@@ -139,10 +148,8 @@ def _store_variant_cad(
         _store_variant_cad_payload(payload_dict, payload_bytes)
 
 
-def _log_exceptions(
-    exceptions: List[W24TechreadException]
-) -> None:
-    """ Log the encountered exceptions as warnings
+def _log_exceptions(exceptions: List[W24TechreadException]) -> None:
+    """Log the encountered exceptions as warnings
 
     Args:
         exceptions (List[W24TechreadException]): List of
@@ -153,10 +160,9 @@ def _log_exceptions(
 
 
 def _store_variant_cad_payload(
-    payload_dict: Dict[str, Any],
-    payload_bytes: bytes
+    payload_dict: Dict[str, Any], payload_bytes: bytes
 ) -> None:
-    """ Store the CAD file with the knowledge that
+    """Store the CAD file with the knowledge that
     the payload exists
 
     Args:
@@ -164,7 +170,7 @@ def _store_variant_cad_payload(
         payload_bytes (bytes): CAD response
     """
     # make the filename
-    variant_id = payload_dict.get('variant_id')
+    variant_id = payload_dict.get("variant_id")
     filename = f"./w24_ask_variant_cad_{variant_id}.dxf"
 
     # and write the content
@@ -175,10 +181,8 @@ def _store_variant_cad_payload(
     logger.info(f"CAD response stored in {filename}")
 
 
-def _get_drawing(
-    file_path: str
-) -> Optional[bytes]:
-    """ Get the bytes of the file that shall be
+def _get_drawing(file_path: str) -> Optional[bytes]:
+    """Get the bytes of the file that shall be
     read.
 
     Args:
@@ -195,11 +199,8 @@ def _get_drawing(
         return None
 
 
-def _print_payload(
-    log_text: str,
-    payload_dict: Dict[str, Any]
-) -> None:
-    """ Display the payload dictionary in a format that
+def _print_payload(log_text: str, payload_dict: Dict[str, Any]) -> None:
+    """Display the payload dictionary in a format that
     is easy for humans to read
 
     Args:
@@ -211,11 +212,8 @@ def _print_payload(
     print(payload_json)
 
 
-def _show_image(
-        log_text: str,
-        image_bytes: bytes
-) -> None:
-    """ Display a debug image. The function relies on
+def _show_image(log_text: str, image_bytes: bytes) -> None:
+    """Display a debug image. The function relies on
     the PIL functionality of Image.show(). The
     actual behavior will thus be different across platforms
 
@@ -234,7 +232,8 @@ def _show_image(
     except ImportError:
         logger.warning(
             "Viewing image-like responses requires the installation"
-            " of the PIL package: pip install pillow. Image skipped.")
+            " of the PIL package: pip install pillow. Image skipped."
+        )
         return
 
     # otherwise print the text
@@ -246,14 +245,12 @@ def _show_image(
         image.show(title=log_text)
     except BaseException:
         logger.warning(
-            "Image cannot be displayed. "
-            "Please check your local security settings.")
+            "Image cannot be displayed. " "Please check your local security settings."
+        )
         return
 
 
-async def main(
-    args: argparse.Namespace
-) -> None:
+async def main(args: argparse.Namespace) -> None:
     """
     Run the Techread command with the CLI arguments
 
@@ -282,7 +279,6 @@ async def main(
     client = utils.make_client()
 
     async with client as session:
-
         # get the drawing
         drawing_bytes = _get_drawing(args.input_file)
         if drawing_bytes is None:
@@ -291,32 +287,31 @@ async def main(
         # and make the request
         try:
             await session.read_drawing_with_hooks(
-                drawing_bytes,
-                hooks,
-                sub_account=sub_account)
+                drawing_bytes, hooks, sub_account=sub_account
+            )
 
         except RequestTooLargeException:
-            message = "Request was too large to be processed. " \
+            message = (
+                "Request was too large to be processed. "
                 + "Please check the documentation for current limits."
+            )
             logger.error(message)
 
 
-def _make_hooks_from_args(
-        args: argparse.Namespace
-) -> List[Hook]:
-
+def _make_hooks_from_args(args: argparse.Namespace) -> List[Hook]:
     # add a general hook to deal with internal errors
     hook_error_internal = Hook(
         message_type=W24TechreadMessageType.ERROR,
         message_subtype=W24TechreadMessageSubtypeError.INTERNAL,
-        function=lambda msg:
-        logger.error("Internal Error %s", msg.payload_dict))
+        function=lambda msg: logger.error("Internal Error %s", msg.payload_dict),
+    )
 
     # add a general hook to deal with PROGRESS messages
     hook_progress_started = Hook(
         message_type=W24TechreadMessageType.PROGRESS,
         message_subtype=W24TechreadMessageSubtypeProgress.STARTED,
-        function=lambda msg: logger.info("Techread process started"))
+        function=lambda msg: logger.info("Techread process started"),
+    )
 
     # tell the api what asks you are interested in,
     # and define what to do when you receive the result
@@ -324,9 +319,16 @@ def _make_hooks_from_args(
         Hook(ask=c.ask(), function=c.function)
         for c in hook_config
         if getattr(args, c.arg)
-    ] + [
-        hook_error_internal,
-        hook_progress_started
-    ]
+    ] + [hook_error_internal, hook_progress_started]
+
+    # take special care of teh W24AskPartFamilyCharacterization
+    if args.part_family_id is not None:
+        c_hook = Hook(
+            ask=W24AskPartFamilyCharacterization(part_family_id=args.part_family_id),
+            function=lambda m: _print_payload(
+                "Part Family Characterization", m.payload_dict
+            ),
+        )
+        hooks.append(c_hook)
 
     return hooks
--- werk24/cli/w24cli.py
+++ werk24/cli/w24cli.py
@@ -11,11 +11,10 @@ load_dotenv(".werk24")
 
 
 def main() -> None:
-
     parser = argparse.ArgumentParser(prog="w24cli")
     subparsers = parser.add_subparsers(
-        dest="service",
-        help="Service. Currently supported: techread, auth, support")
+        dest="service", help="Service. Currently supported: techread, auth, support"
+    )
     subparsers.required = True
 
     _add_auth_parser(subparsers)
@@ -34,140 +33,144 @@ def main() -> None:
 
 
 def _add_support_parser(subparsers):
-    support_parser = subparsers.add_parser(
-        "support",
-        help="Support Service")
+    support_parser = subparsers.add_parser("support", help="Support Service")
 
     support_subparsers = support_parser.add_subparsers(
         dest="command",
         help="Command. Currently supported: create-helpdesk-task",
-        required=True)
+        required=True,
+    )
 
     create_parser = support_subparsers.add_parser(
-        "create-helpdesk-task",
-        help="Create a new help desk task")
+        "create-helpdesk-task", help="Create a new help desk task"
+    )
 
-    create_parser.add_argument(
-        "--request-id",
-        action="store",
-        required=True)
+    create_parser.add_argument("--request-id", action="store", required=True)
 
-    create_parser.add_argument(
-        "--observed-outcome",
-        action="store",
-        required=True)
+    create_parser.add_argument("--observed-outcome", action="store", required=True)
 
-    create_parser.add_argument(
-        "--expected-outcome",
-        action="store",
-        required=True)
+    create_parser.add_argument("--expected-outcome", action="store", required=True)
 
-    create_parser.add_argument(
-        "--comment",
-        action="store")
+    create_parser.add_argument("--comment", action="store")
 
-    create_parser.add_argument(
-        "--importance",
-        action="store",
-        required=True)
+    create_parser.add_argument("--importance", action="store", required=True)
 
 
 def _add_auth_parser(subparsers):
     parser_auth = subparsers.add_parser(
-        "auth",
-        help="Interact with the authentication service")
+        "auth", help="Interact with the authentication service"
+    )
 
     parser_auth.add_argument(
-        '--ask-jwt-token',
-        help="Obtain a valid JWT token",
-        action="store_true")
+        "--ask-jwt-token", help="Obtain a valid JWT token", action="store_true"
+    )
 
 
 def _add_techread_parser(subparsers):
     parser_techread = subparsers.add_parser(
-        "techread",
-        help="Submit a Technical Drawing to Werk24 for analysis")
+        "techread", help="Submit a Technical Drawing to Werk24 for analysis"
+    )
 
     parser_techread.add_argument(
-        "input_file",
-        help="path to the file that is to be analyzed")
+        "input_file", help="path to the file that is to be analyzed"
+    )
 
     parser_techread.add_argument(
         "--sub-account",
         help="Sub-Account that the request should be attributed to.",
         action="store",
-        dest='sub_account',
-        default=None)
+        dest="sub_account",
+        default=None,
+    )
 
     parser_techread.add_argument(
         "--ask-techread-started",
         help="ask for a callback when the techread process has been picked up by a worker",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-page-thumbnail",
         help="ask for a thumbnail for each page in the document",
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-sheet-thumbnail",
         help="ask for a thumbnail of each sheet in the document",
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-sheet-anonymization",
         help="ask for an anonymized of each sheet in the document",
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-canvas-thumbnail",
         help="ask for a thumbnail of each canvas in the document",
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-sectional-thumbnail",
         help="ask for a thumbnail of each sectional of each sheet in the document",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-variant-cad",
         help="ask for the CAD file of each variant",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-variant-external-dimensions",
         help="ask for the External Dimensions of each variant",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-variant-gdts",
         help="ask for the GD&Ts of each variant",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-variant-measures",
         help="ask for the measures of each variant",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-variant-roughnesses",
         help="ask for the Surface Roughnesses of each variant",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-variant-radii",
         help="ask for the Radii of each variant",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
         "--ask-variant-thread-elements",
         help="ask for the Thread Elements",  # noqa
-        action="store_true")
+        action="store_true",
+    )
 
     parser_techread.add_argument(
-        "--ask-titleblock",
-        help="ask for the Title Block",  # noqa
-        action="store_true")
+        "--ask-titleblock", help="ask for the Title Block", action="store_true"  # noqa
+    )
+
+    parser_techread.add_argument(
+        "--ask-part-family-characterization",
+        help="ask for the Part Family Characterization",  # noqa
+        action="store",
+        dest="part_family_id",
+    )
 
 
 if __name__ == "__main__":
