75e2be525ddbf66444ca1eb2fe8125b8769bf092
Aadish <aadish.jain@werk24.io>
2023-10-09 11:28:38 +0200

    adds BOM model

---
+++ werk24/models/bom_table.py
@@ -0,0 +1,59 @@
+from datetime import date
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from werk24.models.revision_table import W24GridSquare
+
+
+class W24BomTableRow(BaseModel):
+    """ Row of a BOM table
+
+    Attributes:
+        serial (Optional[str]): Serial number of the part.
+
+        quantity (Optional[str]): Quantity of the part. 
+
+        bom_position (Optional[str]): Position of the part on the 
+            assembly. 
+
+        part_number (Optional[str]): Part Number or Item Number of 
+            the part listed in the bill of material.
+
+        designation Optional[str]: Designation/Title of the part
+
+        bom_material (str): Material of the part given in the 
+            bill of material.
+
+        weight (str): Weight of the part given in the bill of 
+            material.
+
+        grid_squares: (List[W24GridSquare]): List of the grid squares
+            that are affected by the bom.
+    """
+    serial: Optional[str]
+
+    quantity: str
+
+    bom_position: Optional[str]
+
+    part_number: Optional[str]
+
+    designation: str
+
+    bom_material: Optional[str]
+
+    weight: str
+
+    grid_squares: List[W24GridSquare]
+
+
+class W24BomTable(BaseModel):
+    """ BOM Table object that contains all the
+    rows
+
+    Attributes:
+        rows (List[W24bomTableRow]): List of all
+            rows in the bom table
+    """
+    rows: List[W24BomTableRow]
