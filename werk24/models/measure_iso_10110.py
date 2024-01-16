cd0f87e20634c6da42a1c90d88850ddb55afcd5e
Aadish <aadish.jain@werk24.io>
2024-01-16 16:21:07 +0100

    cq

--- werk24/models/measure_iso_10110.py
+++ werk24/models/measure_iso_10110.py
@@ -1,5 +1,6 @@
 """ Data Model for measures according to ISO 10110
-    Mainly used in optics industry.
+    or DIN 3140 or similar standards. Mainly used 
+    in optics industry.
 
 Author: Jochen Mattes
 """
@@ -8,8 +9,10 @@ from werk24.models.base_feature import W24BaseFeatureModel
 
 
 class W24MeasureIso10110(W24BaseFeatureModel):
-    """Object for measures following ISO 10110.
-
+    """Object for measures following ISO 10110
+        or DIN 3140.For example: 
+        ISO 10110 : 6/4/4/6 2
+        DIN 3140 : 3/ 2(1) according to DIN 3140
     Attributes:
         blurb (str): Raw text that was read.
     """
