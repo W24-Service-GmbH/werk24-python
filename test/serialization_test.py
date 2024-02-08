7b3cb26c790cdfb67b38cd261a5efb000b21b2b9
Aadish <aadish.jain@werk24.io>
2024-02-08 15:04:54 +0100

    cq

--- test/serialization_test.py
+++ test/serialization_test.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from test.utils import AsyncTestCase
 
 from werk24.models.radius import W24CurvatureType, W24RadiusLabel
-from werk24.models.size import W24Size
+from werk24.models.size import W24Size, W24SizeType
 
 
 class TestSerialization(AsyncTestCase):
@@ -11,7 +11,11 @@ class TestSerialization(AsyncTestCase):
     def test_infinity_serialization(self):
         obj = W24RadiusLabel(
             curvature_type=W24CurvatureType.PLANE,
-            size=W24Size(nominal_size=Decimal("Infinity")),
+            size=W24Size(
+                blurb="",
+                size_type=W24SizeType.NOMINAL,
+                nominal_size=Decimal("Infinity"),
+            ),
         ).dict()
         des = W24RadiusLabel.parse_obj(obj)
         self.assertEqual(type(des), W24RadiusLabel)
