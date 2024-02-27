2d3ba5e5545cdb651c07462a39b65c8b937b25c8
werk24 <jochen.mattes@werk24.biz>
2024-02-27 12:03:54 +0100

    cq

--- werk24/models/size.py
+++ werk24/models/size.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from werk24.models.unit import W24UnitLength
 
 
@@ -45,12 +45,9 @@ class W24Size(BaseModel, abc.ABC):
         the units are unknown.
     """
 
-    class Config:
-        json_encoders = {Decimal: lambda v: str(v)}
-
     blurb: str
     size_type: W24SizeType
-    nominal_size: Decimal
+    nominal_size: Decimal = Field(allow_inf_nan=True)
     unit: Optional[W24UnitLength] = None
 
 
