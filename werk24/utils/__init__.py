4a3f23e12a2a921c61bbffb36bbb67adca76c2f9
Jochen Mattes <jochen.mattes@werk24.io>
2025-02-27 09:45:51 +0100

    fixes imports

--- werk24/utils/__init__.py
+++ werk24/utils/__init__.py
@@ -1,1 +1,3 @@
-from .assets import *
+from .assets import get_test_drawing as get_test_drawing
+from .assets import read_drawing_sync as read_drawing_sync
+from .assets import read_example_drawing as read_example_drawing
